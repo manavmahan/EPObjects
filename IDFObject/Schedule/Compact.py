8e9d1031fd7708b4fff6f032537b3d04be91198e
manavmahan <manav.mahan.singh@live.in>
2023-03-09 15:53:14 +0000

    working code with version 22.2

--- IDFObject/Schedule/Compact.py
+++ IDFObject/Schedule/Compact.py
@@ -12,8 +12,17 @@ class Compact(IDFObject):
     def __init__(self, propertiesDict: dict()):
         super().__init__(self.Properties, propertiesDict)
 
+    def ChangeValues(values):
+
+
 Compact.HeatingCoolingSeason = dict(
     Name = 'HeatingCoolingSeason',
     ScheduleTypeLimitsName = ScheduleTypeLimits.AnyNumber['Name'],
     Fields = 'Through: 5/23,For: Alldays,Until: 24:00,1,Through: 7/30,For: Alldays,Until: 24:00,2,Through: 12/31,For: Alldays,Until: 24:00,1',
+)
+
+Compact.OfficePeople = dict(
+    Name = 'People.Office',
+    ScheduleTypeLimitsName = ScheduleTypeLimits.AnyNumber['Name'],
+    Fields = f'Through 12/31, For: Mondays Tuesdays Wednesday Thursdays, Until: 07:30, value0, '
 )
