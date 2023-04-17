a4fa090cc3b1d1cbe741739a2ae8671d17c2cc71
manavmahan <manav.mahan.singh@live.in>
2023-04-18 00:09:23 +0200

    json input to URL

--- WebServer/visualisation_app/src/pages/index.js
+++ WebServer/visualisation_app/src/pages/index.js
@@ -5,6 +5,7 @@ import * as ReactDOM from 'react-dom/client';
 import { Inter } from 'next/font/google'
 
 import { useState, useEffect, useReducer } from 'react';
+import { useRouter } from 'next/router';
 
 import GenerateBuildingElements from './plotly_visualisation';
 
@@ -14,16 +15,19 @@ const URL = 'http://127.0.0.1:3001/'
 
 const inter = Inter({ subsets: ['latin'] })
 
-async function _getGeometry(setGeometry,){
+async function _getGeometry(setGeometry, project_name, user_name){
   let query = {
     "TYPE": "SEARCH", 
     "TABLE_NAME": "PROJECTS", 
     "COLUMN_NAMES": "GEOMETRY", 
-    "CONDITIONS": "PROJECT_NAME='SHAPE_100_000100' AND USER_NAME='RANDOM'",
+    "CONDITIONS": `PROJECT_NAME='${project_name}' AND USER_NAME='${user_name}'`,
   }
+
+  console.log(query);
   
   axios.post(URL, query)
   .then(({data})=>{
+    console.log(data)
     setGeometry(data.RESULT[0][0]);
   })
   .catch((err)=> {
@@ -31,11 +35,13 @@ async function _getGeometry(setGeometry,){
   });
 }
 
-function PlotlyPlot(elementId){
+// http://localhost:3000/?user_name=MANAV&project_name=TAUSENDPFUND
+// http://localhost:3000/?user_name=RANDOM&project_name=SHAPE_100_000155
+function PlotlyPlot(elementId, project_name='TAUSENDPFUND', user_name='MANAV'){
   const [geom, setGeom] = useState(null);
   const [root, setRoot] = useState(null);
 
-  _getGeometry(setGeom);
+  _getGeometry(setGeom, project_name, user_name);
   useEffect(()=>{
     if (document && (!root)) {
       console.log(root);
@@ -56,7 +62,13 @@ function DivElement({ id }) {
 }
 
 function Home() {
-  PlotlyPlot('plot_div');
+  const router = useRouter();
+
+  const { project_name, user_name } = router.query;
+  console.log(router.query);
+  console.log(project_name, user_name)
+
+  PlotlyPlot('plot_div',project_name, user_name);
   return (
     <DivElement id='plot_div' />
   )
