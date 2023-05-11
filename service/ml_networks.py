0bde9c5390e4f3ac4ae82c1e8507f5ab37caa6be
manavmahan <manav.mahan.singh@live.in>
2023-05-11 11:43:25 +0000

    expanded regressor settings

--- service/ml_networks.py
+++ service/ml_networks.py
@@ -1,25 +1,13 @@
 from service import logger, os, np, pd, shutil, tmp_dir
 from MLModels.ml_models import get_regressor, get_generator, predict, get_scaling_parameters
 
-def train_regressor(info, probabilistic_parameters, sampled_parameters: np.ndarray, target_values: np.ndarray):
+def train_regressor(info, probabilistic_parameters, sampled_parameters: np.ndarray, target_values: np.ndarray, HYPERPARAMETERS: pd.DataFrame, NUMS: int, **kwargs):
     logger.info(f'{info}Training Regressor')
     
-    col = ["num_neurons", "reg_coeff", "learning_rate",]
-    num_neurons1 = [20, 50, 100, 200]
-    num_neurons2 = [0, 5, 10, 20]
-    reg_coeff = [1e-3, 1e-4, 1e-5,]
-    learnig_rate = [1e-2, 3e-3, 1e-3,]
-
-    num_neurons = [[nn1, nn2] for nn1 in num_neurons1 for nn2 in num_neurons2]
-    hyperparameters = pd.DataFrame([[n, r, l,] 
-                                        for n in num_neurons
-                                        for r in reg_coeff
-                                        for l in learnig_rate
-                                    ], columns = col).sample(n=2,)
+    hyperparameters = pd.DataFrame.from_dict(HYPERPARAMETERS).sample(n=NUMS,)
     hyperparameters.reset_index(drop=True, inplace=True)
-
     network, loss = get_regressor(hyperparameters, sampled_parameters, target_values, probabilistic_parameters.GetScalingDF(),)
-    
+
     logger.info(f'{info}Regressor Loss:\t{loss:.5f}')
     return network, loss
 
--- service/service.py
+++ service/service.py
@@ -37,7 +37,7 @@ def run_service(user_name, project_name):
         parameters = ProbabilisticParameters.from_df(parameters_df)
 
         regressor_targets = ProbabilisticEnergyPrediction.from_json(simulation_results).Values["Total"]
-        network, loss = train_regressor(info, parameters, sampled_parameters, regressor_targets)
+        network, loss = train_regressor(info, parameters, sampled_parameters, regressor_targets, **project_settings[db.REGRESSOR_SETTINGS])
         db.update_columns(search_conditions, db.REGRESSOR, {db.NETWORK: network, db.LOSS: loss, db.WEIGHTS: network.get_weights()},)
         db.update_columns(search_conditions, db.SCALING, {db.PARAMETERS: parameters.GetScalingDF(), db.PREDICTIONS: get_scaling_parameters(regressor_targets, all_columns_equal=True)},)
 
